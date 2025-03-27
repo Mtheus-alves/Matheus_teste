19c88c242a91ad85ac817eb2442f26b03defe96e
Matheus Correia <matheus.correia2100@gmail.com>
2025-03-27 01:29:33 -0300

    FEAT(BACKEND) --> Criando estrutura base para o backend

---
+++ matheus_teste_backend/matheus/src/main/kotlin/teste/matheus/MatheusApplication.kt
@@ -0,0 +1,11 @@
+package teste.matheus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatheusApplication
+
+fun main(args: Array<String>) {
+	runApplication<MatheusApplication>(*args)
+}

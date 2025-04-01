510978a08d04d1ebd2c768b814c6739e14dc963c
Matheus Alves Correia <79323973+Mtheus-alves@users.noreply.github.com>
2025-03-31 23:07:05 -0300

    removendo cors para teste

--- matheus_teste_backend/matheus/src/main/kotlin/teste/matheus/Webconfig.kt
+++ matheus_teste_backend/matheus/src/main/kotlin/teste/matheus/Webconfig.kt
@@ -1,1 +1,19 @@
 
+package teste.matheus
+ 
+ import org.springframework.context.annotation.Configuration
+ import org.springframework.web.servlet.config.annotation.CorsRegistry
+ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+ 
+ @Configuration
+ class CorsConfig : WebMvcConfigurer {
+ 
+    override fun addCorsMappings(registry: CorsRegistry) {
+          registry.addMapping("/**") 
+              .allowedOrigins("http://localhost:4200")
+              .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+              .allowedHeaders("*")  
+              .allowCredentials(true) 
+              .maxAge(3600)
+      }
+ }

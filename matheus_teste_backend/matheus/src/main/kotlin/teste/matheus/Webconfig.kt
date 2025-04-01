f2e053e7407fb71496ea1cae2eee2cade5c0ccb4
Matheus Alves Correia <79323973+Mtheus-alves@users.noreply.github.com>
2025-03-31 21:46:50 -0300

    fix(cors) --> Tentando arrumar cors

--- matheus_teste_backend/matheus/src/main/kotlin/teste/matheus/Webconfig.kt
+++ matheus_teste_backend/matheus/src/main/kotlin/teste/matheus/Webconfig.kt
@@ -3,17 +3,14 @@ package teste.matheus
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @Configuration
-@EnableWebMvc
 class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")  
-            .allowedOrigins("http://localhost:4200, https://matheus-teste.vercel.app")
-            .allowedMethods("GET", "POST", "PUT", "DELETE") 
-            .allowedHeaders("*")  
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*")
             .maxAge(3600)
     }
 }

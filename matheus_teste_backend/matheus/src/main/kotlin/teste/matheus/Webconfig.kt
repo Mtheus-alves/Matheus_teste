f85e4470b86bb19c9eacae3865f5549e15f20036
Matheus Alves Correia <79323973+Mtheus-alves@users.noreply.github.com>
2025-03-31 23:02:11 -0300

    Update Webconfig.kt

--- matheus_teste_backend/matheus/src/main/kotlin/teste/matheus/Webconfig.kt
+++ matheus_teste_backend/matheus/src/main/kotlin/teste/matheus/Webconfig.kt
@@ -1,16 +1,1 @@
-package teste.matheus
 
-import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
-@Configuration
-class CorsConfig : WebMvcConfigurer {
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOriginPatterns("*")  // Permite qualquer origem
-            .allowedMethods("*")  // Permite todos os métodos
-            .allowedHeaders("*")  // Permite todos os headers
-            .allowCredentials(true)
-    }
-}
